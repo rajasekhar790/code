34f59eae3430403e0cfbb7cc8bbb6b64c5553ed0
rajasekhar790 <Raja.sekhar790@aol.com>
2023-09-26 15:53:22 +0530

    Update business_logic.py

--- business_logic.py
+++ business_logic.py
@@ -1,47 +1,44 @@
+import os
 import pandas as pd
 import nltk
-from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 class ChatBotLogic:
-    def __init__(self, data_path):
+    def __init__(self):
+        # Load and preprocess the data
+        data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data.xlsx')
         self.data = pd.read_excel(data_path)
-        self.preprocess_data()
-        self.vectorize_data()
 
-    def preprocess_data(self):
-        # Filling gaps in the 'message' column
-        self.data['message'] = self.data['message'].fillna("No Message")
+        # Ensure 'message' column exists
+        if 'message' not in self.data.columns:
+            self.data['message'] = "No Message"
 
-        # Drop NA rows
+        # Ensure 'action' column exists
+        if 'action' not in self.data.columns:
+            self.data['action'] = "No action needed"
+
         self.data.dropna(inplace=True)
         self.data.reset_index(inplace=True, drop=True)
 
-        # Download stopwords
-        nltk.download('stopwords', quiet=True)  # Added quiet=True to suppress download messages
-        self.stop_words = stopwords.words('english')
+        # Combine all columns into a single 'combined_data' column
+        self.data['combined_data'] = self.data[self.data.columns[:]].apply(lambda x: ' '.join(x.dropna().astype(str)), axis=1)
 
-    def vectorize_data(self):
-        self.vectorizer = TfidfVectorizer(stop_words=self.stop_words)
-        self.vectors = self.vectorizer.fit_transform(self.data['message'])
+        # Preprocess the data
+        nltk.download('stopwords', quiet=True)
+        stop_words = nltk.corpus.stopwords.words('english')
+        self.tfidf_vectorizer = TfidfVectorizer(stop_words=stop_words)
+        self.tfidf_matrix = self.tfidf_vectorizer.fit_transform(self.data['combined_data'])
 
     def get_response(self, user_input):
-        # Tokenize user input
-        user_tokens = [word for word in nltk.word_tokenize(user_input) if word.lower() not in self.stop_words]
-        user_input_vectorized = self.vectorizer.transform([' '.join(user_tokens)])
-
-        # Calculate cosine similarity
-        cosine_similarities = cosine_similarity(user_input_vectorized, self.vectors).flatten()
-        best_match_index = cosine_similarities.argmax()
-
-        if cosine_similarities[best_match_index] > 0.2:  # Threshold to filter out low matches
-            return self.data.iloc[best_match_index]['message']
+        # Vectorize the user's input and compute similarity
+        user_input_vector = self.tfidf_vectorizer.transform([user_input])
+        cosine_similarities = cosine_similarity(user_input_vector, self.tfidf_matrix).flatten()
+        related_docs_indices = cosine_similarities.argsort()[:-5:-1]
+        
+        # If we find a match, return the corresponding message
+        if related_docs_indices.size > 0 and cosine_similarities[related_docs_indices[0]] > 0.2:
+            return self.data['message'][related_docs_indices[0]]
         else:
-            return "Sorry, I couldn't find a matching response."
-
-# Example Usage (optional):
-# if __name__ == "__main__":
-#     chatbot = ChatBotLogic('data.xlsx')
-#     response = chatbot.get_response('some user input')
-#     print(response)
+            # Return a default response if no match is found
+            return "We are working on it."
