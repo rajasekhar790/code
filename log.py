50c1ca2b737c1c6d02ea317f1f5582a52b4fd5a2
rajasekhar790 <Raja.sekhar790@aol.com>
2024-01-11 14:06:58 +0530

    Update log.py

--- log.py
+++ log.py
@@ -42,5 +42,26 @@ def index():
 # More of your Flask app code...
 # ...
 
+
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+# Set up logging to file
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            os.path.join(os.getcwd(), 'app.log'),
+            maxBytes=10000000,
+            backupCount=10
+        )
+    ]
+)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
