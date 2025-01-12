847d89db33fcade794dea59ede9e2dec2d5d1d18
rajasekhar790 <Raja.sekhar790@aol.com>
2025-01-12 14:26:38 -0500

    Update dsa.py

--- dsa.py
+++ dsa.py
@@ -26,4 +26,23 @@ def find_duplicates(x):
         if x[i] == x[a] and x[i] not in duplicates:
           duplicates.append(x[i])
   return duplicates
-   
+
+
+#########################################################################################
+
+#using direct list 
+my_list = ["apple", "banana", "cherry"]
+for fruit in my_list:
+    print(fruit)  # Prints each fruit, but not its index
+
+#using range 
+my_list = ["apple", "banana", "cherry"]
+for i in range(len(my_list)):
+    print(my_list[i])  # Access value using index i
+
+#Using Enumerate 
+my_list = ["apple", "banana", "cherry"]
+for index, fruit in enumerate(my_list):
+    print(f"Fruit at index {index}: {fruit}")
+
+###########################################################################
