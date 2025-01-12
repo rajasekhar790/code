ca92220436db0e7008a6053c2bfd10ddfe8a376b
rajasekhar790 <Raja.sekhar790@aol.com>
2025-01-12 12:57:56 -0500

    Update dsa.py

--- dsa.py
+++ dsa.py
@@ -14,3 +14,16 @@ def checkDuplicates(arr):
         else:
            return(f" There are the duplicate elements {dup}")
 #######################################################################################
+# another way of finding the duplicates
+
+def find_duplicates(x):
+  length = len(x)
+  duplicates = []
+
+  for i in range(length):
+    n = i + 1
+    for a in range(n, length):
+        if x[i] == x[a] and x[i] not in duplicates:
+          duplicates.append(x[i])
+  return duplicates
+   
